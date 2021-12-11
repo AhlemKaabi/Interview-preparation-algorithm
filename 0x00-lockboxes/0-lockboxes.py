0eef7194d1ad593ff092c1fdf068a36141f7a0c3
AhlemKaabi <ahlemkaabi1412@gmail.com>
2021-12-11 17:49:06 +0100

    fix edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-	Method that determines if all the boxes can be opened.
+    Method that determines if all the boxes can be opened.
 """
 
 
 def canUnlockAll(boxes):
-	"""
-		Return True if ALL boxes can be opened, else return False.
-	"""
-	unlocked_boxes_index = [0]
+    """
+        Return True if ALL boxes can be opened, else return False.
+    """
+    unlocked_boxes_index = [0]
 
-	for idx in range(len(boxes)):
-		if idx in unlocked_boxes_index:
-			for j in range(len(boxes[idx])):
-				if boxes[idx][j] not in unlocked_boxes_index:
-					unlocked_boxes_index.append(boxes[idx][j])
-					for h in range(len(boxes[boxes[idx][j]])):
-						if boxes[boxes[idx][j]][h] not in unlocked_boxes_index:
-							unlocked_boxes_index.append(boxes[boxes[idx][j]][h])
-		else:
-			return False
-	return True
+    for idx in range(len(boxes)):
+        if idx in unlocked_boxes_index:
+            for j in boxes[idx]:
+                unlocked_boxes_index.append(j)
+                if j < len(boxes):
+                    if boxes[j] != [] and type(boxes[j]) == list:
+                        unlocked_boxes_index.extend(boxes[j])
+                    if boxes[j] == None:
+                        return False
+                    if type(boxes[j]) == int:
+                        unlocked_boxes_index.append(boxes[j])
+        else:
+            return False
+    return True
