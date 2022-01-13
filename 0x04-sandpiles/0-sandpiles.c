56dbfa52872a45a359527682e979b643f12077a8
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-01-13 15:50:23 +0100

    betty style

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -61,10 +61,7 @@ void new_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
-	int sum[3][3];
-	int new[3][3];
-	int type;
+	int i, j, sum[3][3], new[3][3], type;
 
 	for (i = 0; i < 3; i++)
 		for (j = 0; j < 3; j++)
