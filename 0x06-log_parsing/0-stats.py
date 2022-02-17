41f7faef673ed9268c61e0a08285cfb46667ba97
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-02-17 17:47:22 +0100

    refactoring code!

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,42 +1,51 @@
 #!/usr/bin/python3
-"""
-    doc
-"""
+""" Module for parsing logs """
 import sys
 
 
-if __name__ == '__main__':
-    codes = ["200", "301", "400", "401", "403", "405", "404", "500"]
-    count_L = 0
-    data = {"status_code": [],
-            "file_size": 0}
-    final_data = {}
+def check_format(line):
+    """ check if the line format is valid """
+    if "GET /projects/260 HTTP/1.1" in line:
+        return 1
+    else:
+        return 0
+
+
+def print_stats():
+    """ print occurence of each source code """
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    total_sizes = 0
+    nb_lines = 0
     try:
         for line in sys.stdin:
-            if "GET /projects/260 HTTP/1.1" in line:
-                count_L += 1
-                data_list = line.split(" ")[-2:]
-                if data_list[0] and data_list[1]:
-                    if data_list[0] in codes:
-                        data["status_code"].append(data_list[0])
-                    data["file_size"] += int(data_list[1])
-                final_data = {}
-                final_data['File size'] = data["file_size"]
-                for elem in sorted(data["status_code"]):
-                    if elem not in final_data.keys():
-                        final_data[elem] = data["status_code"].count(elem)
-                if count_L == 10:
-                    for key in final_data:
-                        if key == '0':
-                            continue
-                        print(key + ":" , final_data[key])
-                    count_L = 0
-                    # data = {"status_code": [],
-                    # 		"file_size": 0}
+            if check_format(line):
+                nb_lines += 1
+                replaced_l = line.replace("-", " ")
+                splited_line = replaced_l.split(" ")
+                size = splited_line[-1]
+                size = size.rstrip("\n")
+                x = size.isnumeric()
+                if x is False:
+                    pass
+                status_code = splited_line[-2]
+                total_sizes += int(size)
+                if status_code.isdigit():
+                    if int(status_code) in codes.keys():
+                        codes[int(status_code)] += 1
+                        if nb_lines == 10:
+                            print("File size: {:d}".format(total_sizes))
+                            for key, value in sorted(codes.items()):
+                                if value != 0:
+                                    print("{:}: {:}".format(key, value))
+                            nb_lines = 0
     except Exception:
         pass
     finally:
-        if not final_data:
-            print("File size:" , data["file_size"])
-        for key in final_data:
-            print(key + ":" , final_data[key])
+        print("File size: {:d}".format(total_sizes))
+        for key, value in sorted(codes.items()):
+            if value != 0:
+                print("{:}: {:}".format(key, value))
+
+
+if __name__ == '__main__':
+    print_stats()
