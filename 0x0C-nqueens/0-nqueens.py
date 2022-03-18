142e84a486b344ef4d97ac3a9e32a3cea736c661
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-03-18 10:58:04 +0100

    try-catch concept added

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -11,12 +11,20 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
+try:
+    int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+
 if not isinstance(int(sys.argv[1]), int):
     print("N must be a number")
     exit(1)
+
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
+
 
 n = int(sys.argv[1])
 
