17e63da9853ac55e6c402fa82b2f37a9b1a2932e
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-04-14 20:44:55 +0100

    update rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -22,23 +22,23 @@ def rain(walls):
 
     # To store the maximum water
     # that can be stored
-    res = 0;
+    res = 0
 
     # For every element of the array
-    for i in range(1, n - 1) :
+    for i in range(1, n - 1):
 
         # Find the maximum element on its left
-        left = walls[i];
-        for j in range(i) :
-            left = max(left, walls[j]);
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
 
         # Find the maximum element on its right
-        right = walls[i];
+        right = walls[i]
 
-        for j in range(i + 1 , n) :
-            right = max(right, walls[j]);
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
 
         # Update the maximum water
-        res = res + (min(left, right) - walls[i]);
+        res = res + (min(left, right) - walls[i])
 
-    return res;
+    return res
