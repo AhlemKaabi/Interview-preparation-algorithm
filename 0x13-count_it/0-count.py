84f2ff8dd7a6f8609db7fdddeb8b575bd1d40575
AhlemKaabi <ahlemkaabi1412@gmail.com>
2022-06-04 13:11:33 +0100

    count it

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,106 +1,37 @@
-#!/usr/bin/python3
-"""
-    Querie the Reddit API
-"""
-import itertools
 import requests
-# Libraries imported in your Python files must be organized in alphabetical
-# order
-
-# initiate a result dictionary
-result_dict = {}
 
 
-def sort_dictionary(result_dict):
-    """
-	sort the dictionary
-    """
-    sorted_by_values = sorted(result_dict.items(),
-                              key=lambda kv: kv[1],
-                              reverse=True)
+def count_words(subreddit, word_list, count_dict={}, after=""):
+    """reddit API"""
+    if len(count_dict) <= 0:
+        for item in word_list:
+            count_dict[item] = 0
 
-    sorted_dict = []
-    for _, v in itertools.groupby(sorted_by_values, lambda item: item[1]):
-        sorted_dict.extend(sorted(v))
+    if after is None:
+        word_list = [[key, value] for key, value in count_dict.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for k in word_list:
+            if k[1]:
+                print("{}: {}".format(k[0].lower(), k[1]))
+        return None
+    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
+    params = {'limit': 100, 'after': after}
+    headers = {'user-agent': 'my-app/0.0.1'}
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False)
 
-    return dict(sorted_dict)
-
-
-def get_reddit(subreddit):
-    """
-    Make the Request
-    """
-    try:
-        base_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&t='all'"
-        request = requests.get(base_url, headers={'User-agent': 'my-app/0.0.1'}, allow_redirects=False)
-    except:
-        print('An Error Occured')
-    return request.json()
-
-
-def get_post_titles(r):
-    '''
-    Get a List of post titles
-    '''
-    posts = []
-    for post in r['data']['children']:
-        x = post['data']['title']
-        posts.append(x)
-    return posts
-
-
-def count_words(subreddit, word_list):
-    """
-    Method:
-        to querie the Reddit API, parse the title of all hot articles,
-        and print a sorted count of given keywords (case-insensitive,
-        delimited by spaces. Javascript should count as javascript,
-        but java should not).
-
-
-    Parameters:
-        subreddit:
-        word_list:
-
-    Returns:
-        prints a sorted count of given keywords.
-        (case-insensitive, delimited by spaces)
-
-    """
-    global result_dict
-    if len(word_list) == 0:
-        for k, v in result_dict.items():
-            if v != 0:
-                print("{}: {}".format(k, v))
-        return
+    if response.status_code == 200:
+        after = response.json().get("data").get("after")
+        posts = response.json().get("data").get("children")
+        for child in posts:
+            title = child.get("data").get(
+                "title")
+            lower = [s.lower() for s in title.split(' ')]
+            for word in word_list:
+                count_dict[word] += lower.count(word.lower())
     else:
-        # do the work!
-        request_api = get_reddit(subreddit)
-        titles_list = get_post_titles(request_api)
-
-        # count_python = 0
-        # for title in titles_list:
-        #     count_python  += title.lower().count("python")
-        #     print(count_python)
-
-        # work on the first element of the list
-        word_to_count = word_list[0].lower()
-        for title in titles_list:
-            # save the count result into the dictionary
-            # add or append data to the dictionary
-            if word_to_count in result_dict:
-                prev_count = result_dict[word_to_count]
-            else:
-                prev_count = 0
-            new_count = title.lower().count(word_to_count)
-            # print(word_to_count)
-            # print(title)
-            # print(new_count)
-            result_dict[word_to_count] = new_count + prev_count
-
-        result_dict = sort_dictionary(result_dict)
-        # print(result_dict)
-        # remove the first element from the word_list
-        word_list.pop(0)
-        # recall the function with new wordlist
-        count_words(subreddit, word_list)
+        return None
+    count_words(subreddit, word_list, count_dict, after)
--- 0x13-count_it/README.md
+++ 0x13-count_it/README.md
@@ -3,3 +3,5 @@
 helpful resources
 
 [Reddit API Without API Credentials](https://www.jcchouinard.com/reddit-api-without-api-credentials/)
+
+[Converting Iterative Code to Recursive Code](https://www.educative.io/collection/page/6151088528949248/4547996664463360/6292303276670976)
